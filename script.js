cddbbb259375f73b21999bae3a3d947ae0688332
mateocordobatoro <cordobatoro.mateo@gmail.com>
2024-10-13 16:57:36 -0500

    portfolio update, style and content

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// Smooth scrolling effect for navigation links
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
-  
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
